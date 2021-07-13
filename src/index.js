80909c72a9ce3035f88cf57a2df5961fa4104166
DrusMarkonii <andrey8566ok@gmail.com>
2021-07-14 00:38:28 +0300

    feat: added jquery

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import * as $ from 'jquery'
+
 import "@css/style.css";
 import Post from "./model/post";
 import json from "./data";
@@ -6,7 +8,7 @@ import xml from "./data.xml";
 import csv from "./data.csv";
 const post = new Post("Webpack Post title", webpackLogo);
 
-console.log("Post to string:", post.toString());
+$('pre').html(post.toString())
 console.log("JSON", json);
 console.log("XML", xml);
 console.log("CSV", csv);
