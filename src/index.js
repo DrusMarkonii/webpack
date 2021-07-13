06dc6e7e5087f8cfd457803800b04bae9cd59b9e
DrusMarkonii <andrey8566ok@gmail.com>
2021-07-13 23:09:34 +0300

    feat: added img from css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Post from "./post";
 import json from "./data.json";
 import webpackLogo from "./img/WebpackLog.png";
 
-const post = new Post("Webpack Post title");
-console.log(webpackLogo);
+const post = new Post("Webpack Post title", webpackLogo);
+
 console.log("Post to string:", post.toString());
 console.log("JSON", json);
