0173e04d9635019a3309e49197db306324a3af2a
DrusMarkonii <andrey8566ok@gmail.com>
2021-07-13 22:35:32 +0300

    feat: aded file-loader

--- dist/main.c059cc17acc41a214a14.js
+++ dist/main.c059cc17acc41a214a14.js
@@ -30,6 +30,16 @@ eval("\n\n/*\n  MIT License http://www.opensource.org/licenses/mit-license.php\n
 
 /***/ }),
 
+/***/ "./img/WebpackLog.png":
+/*!****************************!*\
+  !*** ./img/WebpackLog.png ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"288287c2bc194be5775e6e5a20074e6c.png\");\n\n//# sourceURL=webpack:///./img/WebpackLog.png?");
+
+/***/ }),
+
 /***/ "./css/style.css":
 /*!***********************!*\
   !*** ./css/style.css ***!
@@ -116,7 +126,7 @@ eval("module.exports = JSON.parse('{\"name\":\"yura\",\"age\":\"23\",\"gender\":
   \******************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./css/style.css */ \"./css/style.css\");\n/* harmony import */ var _post__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./post */ \"./post.js\");\n/* harmony import */ var _data_json__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./data.json */ \"./data.json\");\n\r\n\r\n\r\nconst post = new _post__WEBPACK_IMPORTED_MODULE_1__.default('Webpack Post title');\r\n\r\nconsole.log('Post to string:', post.toString())\r\nconsole.log('JSON', _data_json__WEBPACK_IMPORTED_MODULE_2__)\n\n//# sourceURL=webpack:///./index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./css/style.css */ \"./css/style.css\");\n/* harmony import */ var _post__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./post */ \"./post.js\");\n/* harmony import */ var _data_json__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./data.json */ \"./data.json\");\n/* harmony import */ var _img_WebpackLog_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./img/WebpackLog.png */ \"./img/WebpackLog.png\");\n\r\n\r\n\r\n\r\n\r\nconst post = new _post__WEBPACK_IMPORTED_MODULE_1__.default(\"Webpack Post title\");\r\nconsole.log(_img_WebpackLog_png__WEBPACK_IMPORTED_MODULE_3__.default);\r\nconsole.log(\"Post to string:\", post.toString());\r\nconsole.log(\"JSON\", _data_json__WEBPACK_IMPORTED_MODULE_2__);\r\n\n\n//# sourceURL=webpack:///./index.js?");
 
 /***/ }),
 
@@ -181,6 +191,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -195,6 +217,26 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /************************************************************************/
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "./css/style.css";
 import Post from "./post";
 import json from "./data.json";
-const post = new Post("Webpack Post title");
+import webpackLogo from "./img/WebpackLog.png";
 
+const post = new Post("Webpack Post title");
+console.log(webpackLogo);
 console.log("Post to string:", post.toString());
 console.log("JSON", json);
