d885ba020700e15e3616f54c7039fc11dfcfa72c
DrusMarkonii <andrey8566ok@gmail.com>
2021-07-19 21:04:04 +0300

    feat: added scss less

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import * as $ from 'jquery'
 
-import "@css/style.css";
 import Post from "./model/post";
 import json from "./data";
 import webpackLogo from "./img/WebpackLog.png";
 import xml from "./data.xml";
 import csv from "./data.csv";
 const post = new Post("Webpack Post title", webpackLogo);
+
+
+import "@css/style.css";
+import "./less/style.less";
+import "./scss/style.scss";
+
+
 
 $('pre').addClass('code').html(post.toString())
 console.log("JSON", json);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,38 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const { allowedNodeEnvironmentFlags } = require("process");
-const  copyWebpackPlugin  = require("copy-webpack-plugin");
+const copyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtracPlugin = require("mini-css-extract-plugin");
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const { config } = require("webpack");
+
+console.log("is dev", isDev);
+console.log("is prod", isProd);
+
+// const optimisation = () => {
+//   const config = {
+//     splitChunks: {
+//       chunks: "all",
+//     },
+//   };
+// };
+
+// if (isProd) {
+//   config.minimizer = [
+//     new OptimizeCssAssetsWebpackPlugin( {
+//       cssProcessorPluginOptions: {
+//         preset: ["default", { discardComments: { removeAll: true } }],
+//       }
+//     }),
+//     new TerserWebpackPlugin()
+//   ]
+// return config;
+// // }
+
+// const filename = (ext) => isDev ? `[name].$(ext)`: `[name].[fullhash].$(ext)`;
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -13,24 +44,20 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
+    // filename: filename("js"),
   },
   resolve: {
     extensions: [".js", ".json", ".png", ".jsx", ".ts", ".tsx"],
     alias: {
-      '@':path.resolve(__dirname, 'src'),
-      '@model':path.resolve(__dirname, 'src/model'),
-      '@css':path.resolve(__dirname, 'src/css')
-    }
+      "@": path.resolve(__dirname, "src"),
+      "@model": path.resolve(__dirname, "src/model"),
+      "@css": path.resolve(__dirname, "src/css"),
+    },
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
-  },
-  devServer: {
-    port: 4200
-  },
+  // optimization: optimisation(),
+  // devServer: {
+  //   port: 4200,
+  // },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.html",
@@ -38,17 +65,56 @@ module.exports = {
 
     new CleanWebpackPlugin(),
     new copyWebpackPlugin({
-      patterns: [{
-        from: path.resolve(__dirname, 'src/favicon.png'),
-        to: path.resolve(__dirname, 'dist')
-      }]
-    })
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/favicon.png"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
+    new MiniCssExtracPlugin({
+      // filename: filename("css"),
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          {
+            loader: MiniCssExtracPlugin.loader,
+            options: {
+              publicPath: "",
+            },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtracPlugin.loader,
+            options: {
+              publicPath: "",
+            },
+          },
+          "css-loader",
+          "sass-loader"
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtracPlugin.loader,
+            options: {
+              publicPath: "",
+            },
+          },
+          "css-loader",
+          "less-loader"
+        ],
       },
       {
         test: /\.(png|jpeg|gif|svg|jpg|webp)$/,
